dc79ee02b69dedb077c2d3bb27c7b0bd4b105bb1
Truong, Tien Tien Bill <tientientruong@live.fr>
2024-02-28 13:40:29 -0500

    feat: created the SectionFooter component + started the component for text + image

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,23 +1,34 @@
 import Capsule from "../components/Capsule/Capsule";
+import Image from "next/image";
 import H1 from "../components/H1/H1";
 import Typewriter from "../components/Typewriter/Typewriter";
+import SectionChangeAnimation from "../components/SectionChangeAnimation/SectionChangeAnimation";
 
 export default function Home() {
 
   return (
     <>
-      <div className="bg-blue3 text-white w-full h-full flex justify-center items-center flex-col">
+      <div className="bg-blue-3 text-white w-full h-full flex justify-center items-center flex-col">
         <div className="w-full flex flex-col items-center justify-end h-[96px]">
           <h1 className="text-[3rem] font-bold"><Typewriter text="Tien Tien Bill Truong" delay={100} infinite={true}/></h1>
           <p>Full Stack Web Developper</p>
         </div>
+        <SectionChangeAnimation />
       </div>
-      <div className="bg-blue3 text-white w-full h-full flex justify-center items-center flex-col">
-      <div>
-        <h1>Tien Tien Bill Truong</h1>
-        <p>Full Stack Web Developper</p>
-        <Capsule title="Contact Me"/>
+      <div className="bg-blue-2 text-white w-full h-full flex justify-center items-center flex-col">
+      <div className="flex flex-row">
+        <div className="flex flex-col justify-center items-start">
+          <h1>Tien Tien Bill Truong</h1>
+          <p>Full Stack Web Developper</p>
+          <div>
+            <Capsule title="Contact Me"/>
+            
+          </div>
+
+        </div>
+        <Image src="/picture/figma.png" alt="figma design" width={"500"} height={"500"}></Image>
       </div>
+
     </div>
   </>
   );
--- src/app/components/Capsule/Capsule.tsx
+++ src/app/components/Capsule/Capsule.tsx
@@ -2,6 +2,6 @@ import { CapsuleProps } from "./types"
 
 export default function Capsule({ id, title } : CapsuleProps) {
     return(
-        <div id={id} className="rounded-full px-5 py-1 mr-4 text-black bg-gray1">{title}</div>
+        <div id={id} className="rounded-full px-5 py-1 mr-4 text-black bg-gray-1">{title}</div>
     )
 }
